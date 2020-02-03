e5c84cfbd4b8d554086b0bbec94cc857de1fb238
Alina Mikhailova <alina.mihaylova.01@mail.ru>
2020-02-03 18:10:14 +0700

    while в общей строке условия

--- Lessons/Lesson_2/fibonacci.rb
+++ Lessons/Lesson_2/fibonacci.rb
@@ -1,7 +1,6 @@
 #3. Заполнить массив числами фибоначчи до 100
 array = [0, 1]
-while
-	array[-2] + array[-1] <= 100
+	while array[-2] + array[-1] <= 100
   array << array[-2] + array[-1]
 end
 puts array.to_s
